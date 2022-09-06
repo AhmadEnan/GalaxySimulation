6a356243489c52ee368afe3b42d578cfaf5d2cb9
unknown <AhmadEnan>
2022-09-06 21:17:11 +0200

    Fixed .ignore

---
+++ Assets/Scripts/Generation/AstroObject.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AstroObject : MonoBehaviour
+{
+    // Defining main properties of any astronomical object
+    public int sphereOfInflunece;
+    public int radius;
+    public int rocheLimit;
+    public int hillSphere;
+}
