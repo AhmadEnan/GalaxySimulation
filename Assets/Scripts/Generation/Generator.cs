b18d08c070007c93f66ee86b5969d8eecc68f368
unknown <AhmadEnan>
2022-09-07 22:18:14 +0200

    re-worked the noise generator to use Lehemer equations

--- Assets/Scripts/Generation/Generator.cs
+++ Assets/Scripts/Generation/Generator.cs
@@ -4,26 +4,6 @@ using UnityEngine;
 
 public class Generator : MonoBehaviour
 {
-    public float[,] CreateNoiseMap(int mapwidth, int mapheight, float scale) 
-    {
-        // error check
-        scale = Mathf.Clamp(scale, 0.00001f, Mathf.Infinity);
-
-        // construct our map based on given input
-        float[,] noiseMap = new float[mapwidth, mapheight];
-
-        // generate the map using perlin noise
-        for (int y = 0; y < mapheight; y++) 
-        {
-            for (int x = 0; x < mapwidth; x++)
-            {
-                float _x = x / scale, _y = y / scale;
-                noiseMap[x, y] = Mathf.PerlinNoise(_x, _y);
-            }
-        }
-
-        return noiseMap;
-    }
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/Generation/MapDisplay.cs
+++ Assets/Scripts/Generation/MapDisplay.cs
@@ -4,45 +4,64 @@ using UnityEngine;
 
 public class MapDisplay : MonoBehaviour
 {
-    Generator g;
+    // Generator g;
 
     [SerializeField]
     public Renderer DisplayPlane;
 
     public int Width = 128;
     public int Height = 128;
-    public float scale = 2.3f;
-    void DrawNoiseMap(float[,] noiseMap) 
+    public int Seed = 69;
+
+    Lehmer.Random randNumGen;
+    void DrawNoiseMap() 
     {
-        int width = noiseMap.GetLength(0);
-        int height = noiseMap.GetLength(1);
 
-        Texture2D tex = new Texture2D(width, height);
+        Texture2D mapTexture = new Texture2D(Width, Height);
+        
+        Color[] colorMap = new Color[Width * Height];
 
-        Color[] colorMap = new Color[width * height];
-        for (int y = 0; y < height; y++) 
+        for (int y = 0; y < Height; y++) 
         {
-            for (int x = 0; x < width; x++) 
+            for (int x = 0; x < Width; x++) 
             {
-                colorMap[y * width + x] = Color.Lerp(Color.black, Color.white, noiseMap[x, y]);
+                float num = (float)randNumGen.NextDouble();
+                // Color c = num == 0 ? Color.black : Color.white;
+                // colorMap[y * Width + x] = c;
+                colorMap[y * Width + x] = Color.Lerp(Color.black, Color.white, num);
             }
         }
 
-        tex.SetPixels(colorMap);
-        tex.Apply();
-        DisplayPlane.sharedMaterial.mainTexture = tex;
-        DisplayPlane.transform.localScale = new Vector3(width, 1, height);
+        // int width = noiseMap.GetLength(0);
+        // int height = noiseMap.GetLength(1);
+        // 
+        // Texture2D tex = new Texture2D(width, height);
+        // 
+        // Color[] colorMap = new Color[width * height];
+        // for (int y = 0; y < height; y++) 
+        // {
+        //     for (int x = 0; x < width; x++) 
+        //     {
+        //         colorMap[y * width + x] = Color.Lerp(Color.black, Color.white, noiseMap[x, y]);
+        //     }
+        // }
+        // 
+        mapTexture.SetPixels(colorMap);
+        mapTexture.Apply();
+        DisplayPlane.sharedMaterial.mainTexture = mapTexture;
+        DisplayPlane.transform.localScale = new Vector3(Width, 1, Height);
     }
 
     public void DrawMap() 
     {
-        float[,] map = g.CreateNoiseMap(Width, Height, scale);
-        DrawNoiseMap(map);
+        // float[,] map = g.CreateNoiseMap(Width, Height, scale);
+        DrawNoiseMap();
     }
 
     void Start() 
     {
-        g = GameObject.FindGameObjectWithTag("Generator").GetComponent<Generator>();
-
+        // g = GameObject.FindGameObjectWithTag("Generator").GetComponent<Generator>();
+        randNumGen = new Lehmer.Random(Seed);
+        DrawNoiseMap();
     }
 }
