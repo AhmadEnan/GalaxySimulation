f8979655850f9cb760edda6b42e2322f1614a946
AhmedKhairi2 <112388142+AhmedKhairi2@users.noreply.github.com>
2022-09-11 23:13:09 +0200

    l

--- Assets/Scripts/Generation/MapDisplay.cs
+++ Assets/Scripts/Generation/MapDisplay.cs
@@ -4,33 +4,9 @@ using UnityEngine;
 
 public class MapDisplay : MonoBehaviour
 {
-    // Generator g;
-
+    
     [SerializeField]
     private Renderer DisplayPlane;
-
-    // public void DrawNoiseMap(float[,] map)
-    // {
-    //     int width = map.GetLength(0);
-    //     int height = map.GetLength(1);
-    //     
-    //     Texture2D mapTexture = new Texture2D(width, height);
-    // 
-    //     Color[] colorMap = new Color[width * height];
-    // 
-    //     for (int y = 0; y < height; y++) 
-    //     {
-    //         for (int x = 0; x < width; x++) 
-    //         {
-    //             colorMap[y * width + x] = Color.Lerp(Color.black, Color.white, map[x, y]);
-    //         }
-    //     }
-    // 
-    //     mapTexture.SetPixels(colorMap);
-    //     mapTexture.Apply();
-    //     DisplayPlane.sharedMaterial.mainTexture = mapTexture;
-    //     DisplayPlane.transform.localScale = new Vector3(width, 1, height);
-    // }
 
     public void DrawMap(float [,] map)
     {
@@ -53,12 +29,5 @@ public class MapDisplay : MonoBehaviour
         mapTexture.Apply();
         DisplayPlane.sharedMaterial.mainTexture = mapTexture;
         DisplayPlane.transform.localScale = new Vector3(width, 1, height);
-    }
-
-    void Awake()
-    {
-        // g = GameObject.FindGameObjectWithTag("Generator").GetComponent<Generator>();
-        // randNumGen = new Lehmer.Random(Seed);
-        // DrawNoiseMap();
     }
 }
